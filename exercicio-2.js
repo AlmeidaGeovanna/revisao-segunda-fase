2eef40246b8a2e1ce4fc0114ff63bef786c04ce6
IRIS GEOVANNA DE ALMEIDA SILVA <iris_ga_silva@estudante.sesisenai.org.br>
2026-02-18 20:27:06 -0300

    exercicios

---
+++ exercicio-2.js
@@ -0,0 +1,9 @@
+let A = Number(prompt("Digite o primeiro número: "))
+let B = Number(prompt("Digite o segundo número: "))
+
+let soma = A + B
+let subtracao = A - B
+let multiplicacao = A * B
+let divisao = A / B
+
+console.log (soma , subtracao, multiplicacao , divisao)
